3608bec70d7c671d02cc2b0b984ecca316409d99
msearles25 <skaman4459@gmail.com>
2019-11-15 11:26:26 -0500

    route and displaying data

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,34 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get('https://rickandmortyapi.com/api/character/')
+      .then(resp => {
+        setCharacters(resp.data.results);
+        console.log(resp.data.results);
+      })
+      .catch(err => {
+        console.log('No data recieved', err);
+      })
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <div> 
+        {characters.map(character => (
+          <div key={character.id}>
+            <h2>{character.name}</h2>
+            <img src={character.image}/>
+          </div>
+       ))}
+      </div>
     </section>
   );
 }
